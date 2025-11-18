6697e74ee0b82c25e5b6ae53d449fffc0acce537
Lakshya Mittal <lakshyamittalaka@gmail.com>
2025-11-18 12:54:28 +0530

    Detect timeout / gtimeout command automatically

--- scripts/test-mr-many.sh
+++ scripts/test-mr-many.sh
@@ -5,19 +5,36 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
+runs=$1
+
 trap 'kill -INT -$pid; exit 1' INT
 
-# Note: because the socketID is based on the current userID,
-# ./test-mr.sh cannot be run in parallel
-runs=$1
 chmod +x test-mr.sh
 
-for i in $(seq 1 $runs); do
-    timeout -k 2s 900s ./test-mr.sh &
+# Detect timeout / gtimeout (similar to test-mr.sh)
+TIMEOUT=timeout
+if timeout 2s sleep 1 > /dev/null 2>&1; then
+  :
+else
+  if gtimeout 2s sleep 1 > /dev/null 2>&1; then
+    TIMEOUT=gtimeout
+  else
+    TIMEOUT=""
+    echo '*** Cannot find timeout command; proceeding without timeouts.'
+  fi
+fi
+
+for i in $(seq 1 "$runs"); do
+    if [ -n "$TIMEOUT" ]; then
+        $TIMEOUT -k 2s 900s ./test-mr.sh quiet &
+    else
+        ./test-mr.sh quiet &
+    fi
     pid=$!
     if ! wait $pid; then
-        echo '***' FAILED TESTS IN TRIAL $i
+        echo '***' FAILED TESTS IN TRIAL "$i"
         exit 1
     fi
 done
-echo '***' PASSED ALL $i TESTING TRIALS
+
+echo '***' PASSED ALL "$i" TESTING TRIALS
